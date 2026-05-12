b75ea0110a9350df5a66ffea8584d9642ed02dad
Your Name <your-email@gmail.com>
2026-05-12 22:34:42 +0600

    'Error

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@ import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db(); // এখানে ব্র্যাকেটের ভেতর আপনার ডাটাবেজ নাম দিতে পারেন, যেমন: db('myLibrary')
+const db = client.db(); 
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
